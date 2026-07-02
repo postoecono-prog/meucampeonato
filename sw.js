647cbe0d2e58f02af03333d413ab41e883e88248
postoecono-prog <postoecono@gmail.com>
2026-07-02 06:47:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,30 @@
-const CACHE_NAME = 'tabelas-v1';
+const CACHE_NAME = 'arena-tabelas-v2';
 const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
